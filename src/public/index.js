6297b5d88ae3c24d4c5500c734923e62620bf7a4
claysonAlves12 <128482372+claysonAlves12@users.noreply.github.com>
2024-02-27 16:24:14 -0300

    Update index.js

--- src/public/index.js
+++ src/public/index.js
@@ -5,3 +5,22 @@ elementosVideosTutoriais.forEach(function(tutoriais) {
         tutoriais.classList.toggle('active');
     });
 });
+
+
+var videoIframes = document.querySelectorAll('.videoIframe');
+
+videoIframes.forEach(function(videoIframe) {
+    videoIframe.addEventListener('play', function() {
+        if (window.innerWidth <= 768) {
+            if (videoIframe.requestFullscreen) {
+                videoIframe.requestFullscreen();
+            } else if (videoIframe.mozRequestFullScreen) { 
+                videoIframe.mozRequestFullScreen();
+            } else if (videoIframe.webkitRequestFullscreen) { 
+                videoIframe.webkitRequestFullscreen();
+            } else if (videoIframe.msRequestFullscreen) {
+                videoIframe.msRequestFullscreen();
+            }
+        }
+    });
+});
